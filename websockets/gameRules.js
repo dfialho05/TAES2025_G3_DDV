5efeda1f24a06fcd0a3d4d81675fe40bb4992fc6
David Fialho <164379278+dfialho05@users.noreply.github.com>
2025-11-08 19:06:05 +0000

    plays Done
    Created a class for the cards, and add a file to handle the plays (already added the obrigatory to assist when the deck is empty)

---
+++ websockets/gameRules.js
@@ -0,0 +1,52 @@
+import * as card from "./CardClass.js";
+
+// player1, card1 always refferes to the first one playing the card
+
+const selectPlayWinner = (card1, card2, trump) => {
+  // Verifies if both cards are valid Card objects and if the trump suit is valid
+  if (
+    !(card1 instanceof card.Card) ||
+    !(card2 instanceof card.Card) ||
+    !!suits.hasOwnProperty(trump)
+  ) {
+    throw new Error(
+      "Both parameters must be Card objects and trump suit must be valid",
+    );
+  }
+
+  // If the first card is trump and the second card is not trump, the first card wins
+  if (card1.getSuit() === trump && card2.getSuit() !== trump) {
+    return { winner: 1, card1, card2 };
+  }
+
+  // If the second card is trump and the first card is not trump, the second card wins
+  if (card2.getSuit() === trump && card1.getSuit() !== trump) {
+    return { winner: 2, card1, card2 };
+  }
+
+  // If both cards are of the same suit, the higher value wins
+  if (card1.getSuit() === card2.getSuit()) {
+    return { winner: card1.getRank() > card2.getRank() ? 1 : 2, card1, card2 };
+  }
+
+  // If they are of different suits and neither is trump, the first card wins by default
+  return { winner: card1.getSuit() === trump ? 1 : 2, card1, card2 };
+};
+
+const verifyDeckEmpty = (deck) => {
+  // Verify if deck is empty
+  return deck.length === 0 ? 1 : 0;
+};
+
+const doAPlay = (deck, trump, player1Card, player2Hand, player2Card) => {
+  // Verify if deck is empty and both cards has the same suit
+  if (
+    verifyDeckEmpty(deck) &&
+    player1Card.getSuit() !== player2Card.getSuit()
+  ) {
+    if (player2Hand.some((card) => card.getSuit() === player1Card.getSuit())) {
+      throw new Error("Invalid play");
+    }
+  }
+  return selectPlayWinner(player1Card, player2Card, trump);
+};
